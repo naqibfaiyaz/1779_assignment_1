d820eaabf87f28febeedc897014b40ce7f53de17
naqibfaiyaz <naqib3110@gmail.com>
2023-03-03 23:04:35 -0500

    add hit miss to cloudwatch

--- apps/services/memcache/models.py
+++ apps/services/memcache/models.py
@@ -42,24 +42,24 @@ class knownKeys(db.Model):
 
 # class memcahceRequests(db.Model):
 
-    __tablename__ = 'memcache_hit_miss'
+    # __tablename__ = 'memcache_hit_miss'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type = db.Column(Enum('hit','miss'))
-    known_key = db.Column(db.String(64), nullable=True)
-    created_at = db.Column(db.TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'))
-    updated_at = db.Column(db.TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'))
+    # id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # type = db.Column(Enum('hit','miss'))
+    # known_key = db.Column(db.String(64), nullable=True)
+    # created_at = db.Column(db.TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'))
+    # updated_at = db.Column(db.TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'))
 
-    def __init__(self, **kwargs):
-        for property, value in kwargs.items():
-            # depending on whether value is an iterable or not, we must
-            # unpack it's value (when **kwargs is request.form, some values
-            # will be a 1-element list)
-            if hasattr(value, '__iter__') and not isinstance(value, str):
-                # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
-                value = value[0]
+    # def __init__(self, **kwargs):
+    #     for property, value in kwargs.items():
+    #         # depending on whether value is an iterable or not, we must
+    #         # unpack it's value (when **kwargs is request.form, some values
+    #         # will be a 1-element list)
+    #         if hasattr(value, '__iter__') and not isinstance(value, str):
+    #             # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
+    #             value = value[0]
 
-            setattr(self, property, value)
+    #         setattr(self, property, value)
 
 # class memcacheStates(db.Model):
 
